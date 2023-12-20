03dab67cbfd8d3e61c9b492d8e739925655743b7
DCKAP <sathishksdckap@gmail.com>
2023-12-20 12:28:45 +0530

    first commit

--- script.js
+++ script.js
@@ -12,10 +12,13 @@ let amount_err = document.createElement('p') ; amount_err.innerHTML = '<p >Inval
 function addlist(){
    event.preventDefault()
   pname = form.pro_name.value
-  if(pname.trim().length==0) { 
+  if(pname.trim().length==0 && pamount.trim().length!=0) { 
         namediv.append(name_err)   
-}else if(pamount.trim().length==0){
-    namediv.append(amount_err)
+}else if(pamount.trim().length==0 && pname.trim().length!=0 ){
+    amountdiv.append(amount_err)
+}else{
+    namediv.append(name_err) 
+    amountdiv.append(amount_err) 
 }
 
 console.log('dj');
