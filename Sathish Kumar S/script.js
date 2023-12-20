70f538162add41e7da1f84d0207d6640cc8dc2f9
DCKAP <sathishksdckap@gmail.com>
2023-12-20 12:16:24 +0530

    first commmit

---
+++ Sathish Kumar S/script.js
@@ -0,0 +1,22 @@
+let form = document.getElementById('myform')
+    form.addEventListener('submit',addlist)
+let pname 
+let pamount
+let namediv = document.getElementById('name_div')
+let amountdiv = document.getElementById('name_div')
+
+let name_err = document.createElement('p') ; name_err.innerHTML = '<p >Invalid product Name</p> ' ;name_err.style.color = 'red'
+let amount_err = document.createElement('p') ; amount_err.innerHTML = '<p >Invalid amount</p> ' ;amount_err.style.color = 'red'
+
+
+function addlist(){
+   event.preventDefault()
+  pname = form.pro_name.value
+  if(pname.trim().length==0) { 
+        namediv.append(name_err)   
+}else if(pamount.trim().length==0){
+    namediv.append(amount_err)
+}
+
+console.log('dj');
+}
